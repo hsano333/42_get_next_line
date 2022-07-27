455f6b883bff50fc992634bd3adf89f388a8efac
hsano <hsano@student.42tokyo.jp>
2022-07-27 21:03:00 +0900

    fix norm error

--- get_next_line.c
+++ get_next_line.c
@@ -6,64 +6,35 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 21:15:11 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/27 12:22:01 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/27 21:02:46 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	parse_line(char **p, char **old,  ssize_t read_num,ssize_t i)
+int	parse_line(char **p, char **old, ssize_t read_num, ssize_t i)
 {
 	char	*lf;
 
-	//(*p)[i + read_num] = '\0';
-	
 	lf = ft_memchr(*p, '\n', read_num);
-	//printf("i=%zu,read_num=%zu, p=%s,lf=%s, p[0]=%d,lf[0]=%d,%d\n",i,read_num, *p,lf,*p[0],lf[0],'\n');
 	if (lf)
 	{
-		//printf("test No.1 lf=%s\n",lf);
 		if (lf[1] != '\0')
 			*old = ft_strdup(lf + 1);
 		else
 			*old = NULL;
 		lf[1] = '\0';
-		//printf("old=%s, lf=%s,\n",*old,lf);
-		//printf("old=%s,p=%s\n",*old,*p);
 		return (true);
 	}
-	else if (read_num < 0 && errno != EINTR)
+	else if (read_num < BUFFER_SIZE && i != -1)
 	{
-		//printf("test No.2\n");
-		free(*p);
-		*old = NULL;
-		return (true);
-	}
-	else if (i != -1 && read_num < BUFFER_SIZE)
-	{
-		/*
-		lf = ft_memchr(*p, EOF, read_num);
-		if (lf != NULL)
-			lf[1] = '\0';
-			*/
-		//printf("test No.3 i=%zu,read_num=%zu,lf=%s,p=%s\n",i,read_num,lf,*p);
-		if (i == 0)
-		{
-			//printf("test No.4\n");
-			//printf("");
+		if ((read_num < 0 && errno != EINTR) || i == 0)
 			free(*p);
-			*p = NULL;
-		}
 		else
-		{
-
 			(*p)[i] = '\0';
-		}
-		//*p = NULL;
 		*old = NULL;
 		return (true);
 	}
-	//printf("test No.5\n");
 	return (false);
 }
 
@@ -72,65 +43,48 @@ char	*mange_memory(char *old, size_t size)
 	size_t	old_len;
 	char	*p;
 
-	//printf("mangage_memory No.1 old_len=%zu\n",ft_strlen(old));
 	old_len = ft_strlen(old);
-	//printf("mangage_memory No.2 old_len=%zu\n",ft_strlen(old));
-	//printf("old_len=%zu\n",old_len);
 	p = malloc(size + old_len + 1);
-	//p[size + old_len + 1] = '\0';
-	//printf("mangage_memory No.3 old_len=%zu\n",ft_strlen(old));
 	if (!p)
 	{
 		if (old)
 			free(old);
 		return (NULL);
 	}
-	//printf("mangage_memory No.4 old_len=%zu\n",ft_strlen(old));
 	p = ft_strcpy(p, old);
-	//printf("mangage_memory No.5 old_len=%zu\n",ft_strlen(old));
 	if (old)
 		free(old);
-	//printf("mangage_memory No.6 old_len=%zu\n",ft_strlen(old));
 	return (p);
-
 }
 
 char	*get_line(int fd, char **old, size_t size, size_t i)
 {
-	//size_t	i;
 	char	*p;
 	size_t	old_len;
 	ssize_t	read_num;
 
-	//printf("start get_line No.1 old_len=%zu old=\n",ft_strlen(*old));
 	p = mange_memory(*old, size);
-	//printf("start get_line No.2 old_len=%zu old=%s\n",ft_strlen(*old),p);
 	if (!p)
 		return (NULL);
-	//printf("start get_line No.3 old_len=%zu old=%s\n",ft_strlen(*old),p);
 	old_len = ft_strlen(*old);
-	//printf("start get_line No.4 old_len=%zu old=%s\n",ft_strlen(*old),p);
-	//i = old_len;
 	read_num = BUFFER_SIZE;
 	while (read_num == BUFFER_SIZE)
 	{
 		read_num = read(fd, &(p[i]), BUFFER_SIZE);
-		//printf("read p=%s,i=%zu\n",p,i);
-		i += read_num;
+		if (read_num > 0)
+			i += read_num;
 		if (parse_line(&p, old, read_num, i))
-			break;
+			break ;
 		if (i >= size + old_len - BUFFER_SIZE)
 		{
 			p[i] = '\0';
-			//printf("start get_line No.3 old_len=%zu \n",ft_strlen(p));
 			return (get_line(fd, &p, size, i));
 		}
 	}
-	//printf("p=%s\n",p);
 	return (p);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	size_t		size;
 	char		*tmp;
@@ -145,21 +99,12 @@ char *get_next_line(int fd)
 	else
 		size = MIN_BUFFER_SIZE;
 	tmp = NULL;
-	/*
-	if (old != NULL)
-		printf("old is not NULL\n");
-	else
-		printf("old is NULL\n");
-		*/
 	if (old != NULL && parse_line(&old, &tmp, ft_strlen(old), -1))
 	{
-		//printf("swap No.1 old=%s, tmp=%s\n",old,tmp);
 		swap = tmp;
 		tmp = old;
 		old = swap;
-		return tmp;
+		return (tmp);
 	}
-	//printf("get_line\n");
 	return (get_line(fd, &old, size, 0));
 }
-
