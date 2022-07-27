acacb7a41259ad11e03399dda39a14c9e350d000
hsano <hsano@student.42tokyo.jp>
2022-07-27 19:11:09 +0900

    fix norm error in *.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 21:17:31 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/27 12:24:27 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/27 19:10:45 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define MIN_BUFFER_SIZE 8
 # include <fcntl.h>
 # include <sys/types.h>
 # include <sys/stat.h>
@@ -22,15 +23,10 @@
 # include <unistd.h>
 # include <limits.h>
 # include <errno.h>
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 8
-# endif 
-# define MIN_BUFFER_SIZE 8
 
-char	*ft_strcpy(char *dest, char *src);
-void	*ft_memchr(const void *s, int c, size_t n);
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s1);
+char	*ft_strcpy(char *dest, char *src);
+void	*ft_memchr(const void *s, int c, size_t n);
 size_t	ft_strlen(const char *s);
-
-# endif 
+#endif 
